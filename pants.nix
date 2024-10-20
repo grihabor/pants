9bc7f74f8f25236cb58af2b500d80be08f069a2b
Borodin Gregory <grihabor@gmail.com>
2024-10-20 21:32:57 +0200

    Try fix errors

--- pants.nix
+++ pants.nix
@@ -17,9 +17,22 @@
   pants-engine = stdenv.mkDerivation rec {
     inherit src version;
     pname = "pants-engine";
-    cargoDeps = rustPlatform.importCargoLock ./src/rust/engine/Cargo.lock;
+    cargoDeps = rustPlatform.importCargoLock {
+      lockFile = ./src/rust/engine/Cargo.lock;
+      outputHashes = {
+        "deepsize-0.2.0" = "sha256-E73xdzYfpJASps3yz6sjL48Kimy44F2LvxndWzgV3dU=";
+        "deepsize_derive-0.1.2" = "sha256-E73xdzYfpJASps3yz6sjL48Kimy44F2LvxndWzgV3dU=";
+        "globset-0.4.10" = "sha256-1ucpIHxISBqjvKBAea7o2wSddWiIQr6tBiInk4kg0P0=";
+        "ignore-0.4.20" = "sha256-1ucpIHxISBqjvKBAea7o2wSddWiIQr6tBiInk4kg0P0=";
+        "lmdb-rkv-0.14.0" = "sha256-yj0+3wRQkAyp5EYOe2WQeUt1D/3cXZK0XrH6qcxhaWw=";
+        "lmdb-rkv-sys-0.11.0" = "sha256-c9lKJuE74Xp/sIwSFXFsl2EKffY3oC7Prnglt6p1Ah0=";
+        "notify-5.0.0-pre.15" = "sha256-LG6e3dSIqQcHbNA/uYSVJwn/vgcAH0noHK4x3QQdqVI=";
+        "prodash-16.0.0" = "sha256-Dkn4BmsF1SnSDAoqW5QkjdzGHEq41y7S20Q/DkRCpVQ=";
+        "tree-sitter-dockerfile-0.2.0" = "sha256-UQSdcOWRH1QsFWVgxyx9/E7419Ue5zi79ngWNsWuQBc=";
+      };
+    };
 
-    sourceRoot = "${src.name}/src/rust/engine";
+    sourceRoot = "src/rust/engine";
 
     nativeBuildInputs = [
       python
